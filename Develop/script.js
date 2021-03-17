ff46afe292579e9a0ec584979257cf0a5dc30e5b
Fabio Rodrigues <fabiomiglo@hotmail.com>
2021-03-17 18:22:38 -0400

    Variable arrays added.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+//Adding the various arrays for the different types of characters
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "[", "]", "<", ">"];
+
+//Prompts
+var length = "";
+
+//This is our generatePassword function which is where the magic happens!
+//Here you will se how I put the password together!
+generatePassword() {
+//First we prompt our user how long he/she would like their password to be.
+var length =
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -10,5 +29,3 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
