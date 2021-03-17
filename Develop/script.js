e91a9bffa6e837cc99457d441e168b166d1cbe66
Fabio Rodrigues <fabiomiglo@hotmail.com>
2021-03-17 19:01:10 -0400

    Added method of creating custom array of characters to be used.

--- Develop/script.js
+++ Develop/script.js
@@ -20,22 +20,55 @@ var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", 
 
 //Prompts
 var length = "";
-var includeLowerCase;
-var includeUpperCase;
-var includeNumbers;
-var includeSpecialChars;
+var includeLowerCase = false;
+var includeUpperCase = false;
+var includeNumbers = false;
+var includeSpecialChars = false;
 
 //This is our generatePassword function which is where the magic happens!
 //Here you will se how I put the password together!
 function generatePassword() {
 //First we prompt our user how long he/she would like their password to be.
-  var length = (prompt("How many characters should the password contain?"));
+   var length = (prompt("How many characters should the password contain?"));
 
 //Next we make sure the length of the password is within the criteria and if not, replay the first prompt back to user - Loop
-  while(length <= 7 || length >= 129){
-    alert("Password must be between 8 and 128 characters long. Please enter a valid input.");
-    var length = (prompt("How many characters should the password contain?"));
+   while(length <= 7 || length >= 129){
+     alert("Password must be between 8 and 128 characters long. Please enter a valid input.");
+     var length = (prompt("How many characters should the password contain?"));
+  }
+// Now we want to know what kind of character the user would like to include into his password.
+//For this we will create 4 different prompts to confirm which characters are to be included.
+
+   var includeLowerCase = confirm("Click the OK button if you would like to add lowercase letters to your password")
+   var includeUpperCase = confirm("Click the OK button if you would like to add uppercase letters to your password")
+   var includeNumbers = confirm("Click the OK button if you would like to add numbers to your password")
+   var includeSpecialChars = confirm("Click the OK button if you would like to add special characters to your password")
+
+//If all all values come up as false (or not confirmed). There must be at least 1 true value to get past this while loop. Also, send out an alert for the user to know something is wrong.
+   while(includeLowerCase === false && includeUpperCase === false && includeNumbers === false && includeSpecialChars === false) {
+     alert("You must choose at least one character type")
+     var includeLowerCase = confirm("Click the OK button if you would like to add lowercase letters to your password")
+     var includeUpperCase = confirm("Click the OK button if you would like to add uppercase letters to your password")
+     var includeNumbers = confirm("Click the OK button if you would like to add numbers to your password")
+     var includeSpecialChars = confirm("Click the OK button if you would like to add special characters to your password")
   }
 
+  
+  //OK. Now that our prompts are done, how do we use this information?
+  //Well, were going to create a personalized array of the characters that the user has selected!
+  var userSelection = [];
+  
+  if(includeLowerCase){
+    userSelection = userSelection.concat(lowerCase)
+  }
+  if(includeUpperCase){
+    userSelection = userSelection.concat(upperCase)
+  }
+  if(includeNumbers){
+    userSelection = userSelection.concat(numbers)
+  }
+  if(includeSpecialChars){
+    userSelection = userSelection.concat(specialChars)
+  }
 }
 
