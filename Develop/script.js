cf87ac02d44fb3069eaa5d06410f29e226d8945f
Fabio Rodrigues <fabiomiglo@hotmail.com>
2021-03-17 18:35:21 -0400

    First Prompt added

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 
 //Adding the various arrays for the different types of characters
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -12,20 +20,22 @@ var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", 
 
 //Prompts
 var length = "";
+var includeLowerCase;
+var includeUpperCase;
+var includeNumbers;
+var includeSpecialChars;
 
 //This is our generatePassword function which is where the magic happens!
 //Here you will se how I put the password together!
-generatePassword() {
+function generatePassword() {
 //First we prompt our user how long he/she would like their password to be.
-var length =
-}
+  var length = (prompt("How many characters should the password contain?"));
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+//Next we make sure the length of the password is within the criteria and if not, replay the first prompt back to user - Loop
+  while(length <= 7 || length >= 129){
+    alert("Password must be between 8 and 128 characters long. Please enter a valid input.");
+    var length = (prompt("How many characters should the password contain?"));
+  }
 
 }
 
